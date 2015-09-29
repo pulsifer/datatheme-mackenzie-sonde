b98c35bc0adbe5a2ba7946d8d48410e20c10f646
Boris Cosic <boris.cosic@tesera.com>
2015-09-29 16:30:14 -0600

    DT update.

--- dt2dp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo '{"name": "mackenzie-sonde-dataset",'
-echo '"datatheme": { "name": "mackenzie.sonde", "version": "1.0.2", "repository": "https://github.com/tesera/datatheme-mackenzie-sonde.git"},'
-echo '"resources": ['
-    cat ./schemas/sonde_data.json
-    echo ,
-    cat ./schemas/sonde_meta.json
-echo ']}'
